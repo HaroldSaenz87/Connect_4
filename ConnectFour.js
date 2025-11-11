a590ff7d3235faadf35684568e1795d795373cd7
Harold Saenz <joshu4nick19@gmail.com>
2025-11-10 23:20:30 -0500

    adding the gravity.

--- ConnectFour.js
+++ ConnectFour.js
@@ -6,6 +6,7 @@ let currentPlayer = playerRed;
 
 let gameOver = false;
 let board;
+let currentCol;
 
 let rows = 6;
 let col = 7;
@@ -16,6 +17,7 @@ window.onload = function(){
 
 function setGame(){
     board = [];
+    currentCol = [5, 5, 5, 5, 5];
 
     for(let r = 0; r < rows; r++){
         
@@ -30,8 +32,51 @@ function setGame(){
 
             tile.classList.add("tile");
 
+            tile.addEventListener("click", setPiece);
+
             document.querySelector(".Game").append(tile);
         }
         board.push(row);
     }
+}
+
+function setPiece(){
+
+    if(gameOver){
+        return;
+    }
+
+    let coordinates = this.id.split("-");
+    
+    let r = parseInt(coordinates[0]);
+
+    let c = parseInt(coordinates[1]);
+
+
+    r = currentCol[c];
+
+    if(r < 0){
+
+        return;
+    }
+
+    board[r][c] = currentPlayer;
+
+    let tile = document.getElementById(r.toString() + "-" + c.toString());
+
+    if(currentPlayer == playerRed){
+        
+        tile.classList.add("red_piece");
+
+        currentPlayer = playerYellow;
+    }
+    else{
+        
+        tile.classList.add("yellow_pieces");
+        
+        currentPlayer = playerRed;
+    }
+
+    r -= 1;
+    currentCol[c] = r;
 }
